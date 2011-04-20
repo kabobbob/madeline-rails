ecc226645e350815157782051ab10d36db24610f
Bob McClellan Jr <mcclellan@chgr.mc.vanderbilt.edu>
2011-04-20 10:22:47 -0500

    Starting over to clear out makefiles, etc.

--- 3rdParty/zlib-1.1.4-minizip-0.15/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kabob/workspace/madeline/3rdParty/unzip
-# Build directory: /Users/kabob/workspace/madeline/3rdParty/unzip
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Capfile
+++
@@ -1,5 +0,0 @@
-load 'deploy' if respond_to?(:namespace) # cap2 differentiator
-
-set :stages, %w(staging production dev)
-set :default_stage, "dev"
-require 'capistrano/ext/multistage'
